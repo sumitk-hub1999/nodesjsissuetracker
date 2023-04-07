230831f1f6653bf741cdad0b442d97d9ae71d5d3
sumitk-hub1999 <skatyal1999@gmail.com>
2023-04-07 19:17:05 +0530

    start

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express(); //app initialised as express app
+const path = require("path");
 
 dotenv.config({ path: "config.env" }); //collabarative env we use dotenv
 const port = process.env.port || 8080; //searching in dotenv file
 //log request
 app.use(morgan("tiny"));
+//pass request to body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+//set view engine
+app.set("view engine", "ejs");
+//set views folder where ejs files stored
+//app.set("views", path.resolve(__dirname,""));
+//load assets using middleware metho d use
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/images", express.static(path.resolve(__dirname, "assets/images")));
+app.use("/script", express.static(path.resolve(__dirname, "assets/script")));
 app.get("/", (req, res) => {
-  res.send("Crud Application");
+  res.render("index");
 }); //route
 app.listen(port, () => {
   console.log(`server is running on port http://localhost:${port}`);
